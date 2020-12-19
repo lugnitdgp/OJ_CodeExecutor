57bb35b866877c7e35c23e08f2d466be17d8ff40
Himanshu Shekhar <himanshu.shekharjha.272@gmail.com>
2020-12-20 00:21:37 +0530

    [UPDATE] Requirements

--- OJExec-Python/settings.py
+++ OJExec-Python/settings.py
@@ -1,7 +1,6 @@
 import os
 from decouple import config
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -33,8 +32,7 @@ DATABASES = {
     }
 }
 
-# SECURITY WARNING: Modify this secret key if using in production!
-SECRET_KEY = '6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa'
+SECRET_KEY = config("SECRET_KEY")
 
 FILE_HASHES = {}
 
